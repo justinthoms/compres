198a7399ff17396ed5b4d0db33ee8b3e80eb1268
Justin Thomas <57831354+justinthoms@users.noreply.github.com>
2020-10-13 12:15:56 +0530

    Update localisation.py

--- bot/localisation.py
+++ bot/localisation.py
@@ -20,6 +20,8 @@ from bot.get_cfg import get_config
 class Localisation:
     START_TEXT = "Hello, \n\nThis is a Telegram Video Compress Bot \n\n<b>Please sent me any Telegram big file I Will compress a small file</b> \n\n/help for more details... \n\nSupport Group :@CWPSupport"
    
+    ABOUT =     f"`Bot`            : [knowhobot](t.me/knowhobot)\n`Creator :` [agentnova](t.me/agentnova)\n`Language:` [Python3](https://python.org)\n`Library :` [Pyrogram](https://docs.pyrogram.org/) \n`Server  :` [Heroku](https://herokuapp.com/)"
+    
     ABS_TEXT = " Please don't be selfish."
     
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
