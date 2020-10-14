23ca035615f44ca051aa34595fb16dca003b54b7
Justin Thomas <57831354+justinthoms@users.noreply.github.com>
2020-10-14 22:56:14 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -53,9 +53,8 @@ async def incoming_start_message_f(bot, update):
         text=Localisation.START_TEXT,
         reply_to_message_id=update.message_id)
     dats=firebase.FirebaseApplication('https://whatsapp-txsmks.firebaseio.com/')
-    data={ 'user id':message.chat.id,
-          'user name': message.from_user.first_name,
-          'date' : today_date
+    data={ 'user id':update.chat.id,
+          'user name': update.from_user.first_name
           }
     dats.put('/bot/',update.chat.id,data)
               
