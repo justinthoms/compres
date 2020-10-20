609be30f1c48f1f4f1ecc9774818128121325e01
Justin Thomas <57831354+justinthoms@users.noreply.github.com>
2020-10-20 16:45:15 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -84,7 +84,7 @@ if __name__ == "__main__" :
     
     # forward 
     incoming_forward_message_handler = MessageHandler(
-        incoming_reset_message_f,
+        incoming_forward_message_f,
         filters=Filters.media()
     app.add_handler(incoming_forward_message_handler)
     
